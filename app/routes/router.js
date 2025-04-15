4ffcb9f6f6c77ce95227d151d8b6519463847247
Zeek <ezfirststep@gmail.com>
2025-04-15 11:59:51 -0500

    home page added

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,6 +2,7 @@
 // 1a
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const PORT = process.env.PORT || 3000
 
 // 2
@@ -19,9 +20,40 @@ endpoints.forEach(endpoint => {
 // 3 
 // router.get(path, callback function)
 router.get('/', (req, res)=> {
-    res.render('pages/home', {
-        title: 'Home',
-        name: 'My Hero Website'
+
+    let randomHero = {}
+    let message = ''
+    const url = `http://localhost:${PORT}/api/hero`
+
+    axios.get(url)
+    .then(resp => {
+        randomHero = resp.data[Math.floor(Math.random() * resp.data.length)]
+        // console.log(randomHero)
+
+        let heroName = randomHero.hero_name != null ? randomHero.hero_name : `${randomHero.first_name} ${randomHero.last_name}`
+
+        switch (randomHero.alignment) {
+
+            case 'HERO':
+                message = `Great news! ${heroName} is here to save you!`
+                break;
+            case 'ANTIHERO':
+                message = `I guess you need to get on ${heroName}'s good side if you want to live.`
+                break;
+            case 'VILLAIN':
+                message = `Looks like ${heroName} is here to destroy you and everything you love...`
+                break;
+            default:
+                message = ''
+        }
+        // console.log(message)
+        res.render('pages/home', {
+            title: 'Home',
+            name: 'My Hero Website',
+            randomHero,
+            message,
+            heroName
+        })
     })
 })
 
