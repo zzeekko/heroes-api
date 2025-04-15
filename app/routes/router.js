760a9abe4768b78a34653a894303d4a0a6119fd0
Zeek <ezfirststep@gmail.com>
2025-04-15 14:23:26 -0500

    built single and aside

--- app/routes/router.js
+++ app/routes/router.js
@@ -10,6 +10,12 @@ router.use(express.static('public'))
 
 const endpoints = [ 'hero', 'franchise', 'team', 'power', 'species']
 
+// heroAside data
+let heroAsideData = []
+
+axios.get(`http://localhost:${PORT}/api/hero/sort`)
+.then(resp => heroAsideData = resp.data)
+
 // individual routes
 // router.use('/api/hero', require('./api/heroRoutes'))
 // router.use('/api/franchise', require('./api/franchiseRoutes'))
@@ -53,6 +59,35 @@ router.get('/', (req, res)=> {
             randomHero,
             message,
             heroName
+        })
+    })
+})
+
+// hero page
+router.get('/heroes', (req, res)=> {
+    const url = `http://localhost:${PORT}/api/hero`
+    axios.get(url).then(resp => {
+        res.render('pages/hero', {
+            title: 'All Heroes',
+            name: 'Heroes',
+            data: resp.data,
+            asideData: heroAsideData
+        })
+    })
+})
+
+// hero single
+router.get('/heroes/:id', (req, res)=> {
+    const id = req.params.id
+    const url = `http://localhost:${PORT}/api/hero/${id}`
+    axios.get(url).then(resp => {
+
+        let heroName = resp.data.hero_name == null ? `${resp.data.first_name} ${resp.data.last_name}` : `${resp.data.hero_name}`
+        res.render('pages/heroSingle', {
+            title: heroName,
+            name: heroName,
+            data: resp.data,
+            asideData: heroAsideData
         })
     })
 })
